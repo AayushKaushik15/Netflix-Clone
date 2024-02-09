cedd6ce3d6a7877adb2ee2e7c67bca046a40a0b9
shashank <shashankkaushik106@gmail.com>
2024-02-09 17:28:41 +0530

    signin/signup form using UseState()

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import Header from './Header'
-
+import { useState } from 'react'
 const Login = () => {
+
+  const [isSignInForm, setIsSignFrom]  = useState(true);
+
+  function toggleSignInForm () {
+    setIsSignFrom(!isSignInForm)
+  }
+// Formic {form} library 
   return (
     <div>
         <Header/>
@@ -10,29 +17,39 @@ const Login = () => {
         </div>
         <div className='login-form-main'>
             <form className='login-form'>
-                <h1>Sign In</h1>
+                <h1>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
                   <div className='inputs'>
+                  {
+                    !isSignInForm && <input type="text" name="" id="" placeholder='Full Name' required/>
+                  }
                   <input type="email" name="" id="" placeholder='Email address' required/>
                   <input type="password" name="" id="" placeholder='password' required/>
                   </div>
-                <button>Sign in</button>
+                <button>{isSignInForm ? "Sign In" : "Sign Up"}</button>
                 <p className='or'>OR</p>
                 <p className='sign-in-code'>Use a Sign-in code</p>
-                <p className='forgot'>Forgot password?</p>
+                <p className='forgot' >Forgot password?</p>
                <div className='remember'>
                   <input type="checkbox" />
                   <p>Remember me</p>
                </div>
 
                <div className='last-section-login'>
-                  <p className='para-3'>New to Netflix? <a href="#">Sign up now</a></p>
+                  <p className='para-3'>New to Netflix? <a href="#" onClick={toggleSignInForm}>{isSignInForm ? "sign up" : "sign in"} now</a></p>
                   <p className='para-4'>This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.</p>
                </div>
             </form>
         </div>
-        <div>
-          
-        </div>
+        <footer>
+          <p>Question? Call 000-800-919-1694</p>
+          <div>
+            <ul>
+              <li>FAQ</li>
+              <li>Terms of Use</li>
+              <li>Cookies Preference</li>
+            </ul>
+          </div>
+        </footer>
     </div>
   )
 }
